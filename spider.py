5340840a3de8a4892a80001e90719319394c3629
van0509 <free001@vip.qq.com>
2018-08-14 03:54:24 +0800

    这只是试试看

---
+++ spider.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#coding:utf-8
+
+"""
+@author: Seven°
+@contact: free001@vip.qq.com
+@software: PyCharm
+@file: spider.py
+@time: 2018-08-14 3:50
+"""
+
+
+def main():
+    print('you are my firends')
+
+
+if __name__ == "__main__":
+    main()
