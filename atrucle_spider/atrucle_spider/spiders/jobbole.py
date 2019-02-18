0a60d8a85f70821f97db1d823ae14f1f62a19050
Seven <free001@vip.qq.com>
2019-02-19 01:44:01 +0800

    2019年2月19日01:30:02
    Signed-off-by: Seven <free001@vip.qq.com>

--- atrucle_spider/atrucle_spider/spiders/jobbole.py
+++ atrucle_spider/atrucle_spider/spiders/jobbole.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-import scrapy
 import re
+
+import scrapy
 from scrapy.http import Request
 
 
--- tedu.py
+++ tedu.py
@@ -43,7 +43,7 @@ headers = {
     'Accept-Language': 'zh-CN,zh;q=0.9',
     'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
-    'Cookie': 'num_1=2; eBoxOpenVIP_AID_N_GXZ_V01_S=true; isCenterCookie=no; tedu.local.language=zh-CN; uniqueVisitorId=2bfbf581-fa0d-e1fa-fc69-3b44598de95a; Hm_lvt_51179c297feac072ee8d3f66a55aa1bd=1549033711,1549288721,1549541049,1550296602; Hm_lpvt_51179c297feac072ee8d3f66a55aa1bd=1550296602; TMOOC-SESSION=F2BF7D2F7B454DA8BA923C9BC9C4F5DA; sessionid=F2BF7D2F7B454DA8BA923C9BC9C4F5DA|1317643074%40qq.com; cloudAuthorityCookie=0; versionListCookie=VIP_AID_N_GXZ_V01_S; defaultVersionCookie=VIP_AID_N_GXZ_V01_S; versionAndNamesListCookie=VIP_AID_N_GXZ_V01_SN22NAID%25E5%25B0%25B1%25E4%25B8%259A%25E8%25AF%25BE%25E7%25A8%258BVIP1.0; courseCookie=AID; stuClaIdCookie=546140; Hm_lvt_e997f0189b675e95bb22e0f8e2b5fa74=1549550168,1549550240,1549589006,1550296624; Hm_lpvt_e997f0189b675e95bb22e0f8e2b5fa74=1550296624; JSESSIONID=38E65D877B7022BFC7CED8847C4BFA7A',
+    'Cookie': 'num_1=2; isCenterCookie=no; eBoxOpenVIP_AID_N_GXZ_V01_S=true; tedu.local.language=zh-CN; uniqueVisitorId=2bfbf581-fa0d-e1fa-fc69-3b44598de95a; Hm_lvt_51179c297feac072ee8d3f66a55aa1bd=1549288721,1549541049,1550296602,1550495633; Hm_lpvt_51179c297feac072ee8d3f66a55aa1bd=1550495633; TMOOC-SESSION=E1AD820301ED4CCAA781FBDAE05ED709; sessionid=E1AD820301ED4CCAA781FBDAE05ED709|1317643074%40qq.com; cloudAuthorityCookie=0; versionListCookie=VIP_AID_N_GXZ_V01_S; defaultVersionCookie=VIP_AID_N_GXZ_V01_S; versionAndNamesListCookie=VIP_AID_N_GXZ_V01_SN22NAID%25E5%25B0%25B1%25E4%25B8%259A%25E8%25AF%25BE%25E7%25A8%258BVIP1.0; courseCookie=AID; stuClaIdCookie=546140; Hm_lvt_e997f0189b675e95bb22e0f8e2b5fa74=1549589006,1550296624,1550296634,1550495646; Hm_lpvt_e997f0189b675e95bb22e0f8e2b5fa74=1550495646; JSESSIONID=A4F9A23348CFDA120F9DE04AFA6BAD81',
     'Host': 'tts.tmooc.cn',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
@@ -57,27 +57,28 @@ for url1 in conn[1::]:
     if rep1:
         pattern1 = "\('([\s\S]*?)','A_VIP_[\s\S]*?title=\"([\s\S]*?)\">"
         resp = re.findall(pattern1, rep1)
-        print(resp)
+        # print(resp)
         for i in resp:
             z += 1
         #     # print(i[0],i[1])
-        #     db = myDb.mySql()
-        #     ID= i[0]
-        #     title = i[1]
-        #     sql = 'INSERT INTO seven.python(title, ID)VALUES(%s,%s);'
-        #     params = (title, ID)
-        #     resoult = db.execute(sql, params)
-        #     if resoult == True:
-        #         print('Insert 成功%d条'%z)
-        #     else:
-        #         print('Insert 失败')
-            mac = 'https://p.bokecc.com/servlet/getvideofile?vid=%s&siteid=0DD1F081022C163E&divid=cc_video_%s_9332455&width=1400&useragent=Android&version=20140214&hlssupport=1&vc=tts-110870F5F7644B12BF4C009A2B48DBFA&mediatype=1&callback=cc_js_Player.showPlayerView&r=7395025.59645809' % (
-            i[0], i[0])
-            tts = requests.get(mac).text
-            pattern2 = '"backupurl":"([\s\S]*?)","'
-            down = re.findall(pattern2, tts)
-            with open('download1.txt', 'a') as f:
-                f.writelines(str(z)+'-'+i[1] + ',' + down[0] + '\n')
-            print('当前插入第%d条链接,加油哈！' % z)
+            db = myDb.mySql()
+            ID= i[0]
+            title = i[1]
+            sql = 'INSERT INTO seven.python(title, ID)VALUES(%s,%s);'
+            params = (title, ID)
+            resoult = db.execute(sql, params)
+            if resoult == True:
+                print('Insert 成功%d条'%z)
+            else:
+                print('Insert 失败')
+        #     mac = 'https://p.bokecc.com/servlet/getvideofile?vid=%s&siteid=0DD1F081022C163E&divid=cc_video_%s_9332455&width=1400&useragent=Android&version=20140214&hlssupport=1&vc=tts-110870F5F7644B12BF4C009A2B48DBFA&mediatype=1&callback=cc_js_Player.showPlayerView&r=7395025.59645809' % (
+        #     i[0], i[0])
+        #     tts = requests.get(mac).text
+        #     pattern2 = '"backupurl":"([\s\S]*?)","'
+        #     down = re.findall(pattern2, tts)
+        #     print(down)
+            # with open('download1.txt', 'a') as f:
+            #     f.writelines(str(z)+'-'+i[1] + ',' + down[0] + '\n')
+            # print('当前插入第%d条链接,加油哈！' % z)
 
             # cc_js_Player.showPlayerView({"uid":"0DD1F081022C163E","divid":"cc_video_1E833DCEFCACE45B9C33DC5901307461_9332455","img":"http://2-img.bokecc.com/comimage/0DD1F081022C163E/2018-07-05/1E833DCEFCACE45B9C33DC5901307461-1.jpg","UPID":"4904241549554480614","vrmode":0,"playtype":0,"authenable":1,"freetime":0,"defaultquality":20,"vid":"1E833DCEFCACE45B9C33DC5901307461","copies":[{"mediatype":1,"backupurl":"http://cd15-c120-1.play.bokecc.com/flvs/0DD1F081022C163E/2018-07-05/1E833DCEFCACE45B9C33DC5901307461-20.m3u8?t=1549561680&key=B1E3FC450A468F1600CC6889067A9F73&tpl=10&tpt=112","desp":"高清","playurl":"http://cd12-c120.play.bokecc.com/flvs/0DD1F081022C163E/2018-07-05/1E833DCEFCACE45B9C33DC5901307461-20.m3u8?t=1549561680&key=B1E3FC450A468F1600CC6889067A9F73&tpl=10&tpt=112","quality":20},{"mediatype":1,"backupurl":"http://cd15-c120-1.play.bokecc.com/flvs/0DD1F081022C163E/2018-07-05/1E833DCEFCACE45B9C33DC5901307461-10.m3u8?t=1549561680&key=5636090A09957C10040DEC9805D1F8E6&tpl=10&tpt=112","desp":"清晰","playurl":"http://cd12-c120.play.bokecc.com/flvs/0DD1F081022C163E/2018-07-05/1E833DCEFCACE45B9C33DC5901307461-10.m3u8?t=1549561680&key=5636090A09957C10040DEC9805D1F8E6&tpl=10&tpt=112","quality":10}],"status":1})
